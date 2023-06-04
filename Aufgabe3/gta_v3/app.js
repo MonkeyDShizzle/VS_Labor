58b53784831fef959ebd66ffc233ec25eed8aacc
MMohamme <bassam.sallam.de@gmail.com>
2023-06-04 19:58:31 +0200

    update

--- Aufgabe3/gta_v3/app.js
+++ Aufgabe3/gta_v3/app.js
@@ -63,3 +63,7 @@ app.use(function(err, req, res) {
   });
 
  module.exports = app;
+ const port = process.env.PORT || 3000;
+ app.listen(port, function () {
+   console.log('Server is running on http://localhost:' + port);
+ });
